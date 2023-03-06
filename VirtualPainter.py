b01faba9dac9b0f4486681061fe40323b4086414
Ирина Тюмянцева <irinatyumyantseva@gmail.com>
2023-03-06 22:24:23 +0300

    Added the av virtual painter

--- VirtualPainter.py
+++ VirtualPainter.py
@@ -1,8 +1,12 @@
 import cv2
 import numpy as np
-import time
 import os
 import HandTrackingModule as htm
+
+############
+brushThickness = 15
+eraserThicknesss = 100
+############
 
 # Importing images
 folderPath = "Header"
@@ -13,6 +17,7 @@ for imPath in myList:
     overlayList.append(image)
 # print(len(overlayList))
 header = overlayList[0]
+drawColor = (255, 0, 0)
 
 wCam, hCam = 1280, 720
 
@@ -21,6 +26,10 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 
 detector = htm.handDetector(detectionCon=0.85)
+
+xp, yp = 0, 0
+
+imgCanvas = np.zeros((720, 1280, 3), np.uint8)
 
 while True:
     # Import image
@@ -45,29 +54,53 @@ while True:
 
         # If selection mode - Two fingers are up
         if fingers[1] and fingers[2]:
-            cv2.rectangle(img, (x1, y1 - 30), (x2, y2 + 30), (255, 0, 0), cv2.FILLED)
+            xp, yp = 0, 0
             print("Selection mode")
             # Checking the color
             if y1 < 125:
                 if 250 < x1 < 450:
                     header = overlayList[0]
-                if 550 < x1 < 750:
+                    drawColor = (0, 0, 255)
+                if 500 < x1 < 650:
                     header = overlayList[1]
-                if 800 < x1 < 950:
+                    drawColor = (255, 0, 0)
+                if 700 < x1 < 850:
                     header = overlayList[2]
-                if 1050 < x1 < 1200:
+                    drawColor = (0, 255, 0)
+                if 1000 < x1 < 1200:
                     header = overlayList[3]
+                    drawColor = (0, 0, 0)
+
+            cv2.rectangle(img, (x1, y1 - 30), (x2, y2 + 30), drawColor, cv2.FILLED)
 
         # If drawing mode - Index finger is up
         if fingers[1] and fingers[2] == False:
-            cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, drawColor, cv2.FILLED)
             print("Drawing mode")
+
+            if xp == 0 and yp == 0:
+                xp, yp = x1, y1
+
+            if drawColor == (0, 0, 0):
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, eraserThicknesss)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThicknesss)
+            else:
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+
+            xp, yp = x1, y1
+
+    # Drawing in the canvas
+    imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)  # getting gray image
+    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)  # inverting image
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img, imgInv)  # adding inverse image and main image (showing black image)
+    img = cv2.bitwise_or(img, imgCanvas)  #
 
 
     # SEtting the header image
     img[0:125, 0:1280] = header
+    # img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0)
     cv2.imshow("Image", img)
+    # cv2.imshow("Canvas", imgCanvas)
     cv2.waitKey(1)
-
-
-
